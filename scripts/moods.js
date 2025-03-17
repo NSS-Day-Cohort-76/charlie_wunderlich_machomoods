23259028e98687ca7b82c8aafff20abd6bde30ed
Charlie Wunderlich <cww324@gmail.com>
2025-03-17 15:01:43 -0500

    updated index.html and moods.js to allow more CSS properties to be accesses and completley reworked main.css, now the site works as intended just missing flair

--- scripts/moods.js
+++ scripts/moods.js
@@ -6,13 +6,13 @@ let moodsHTML = '';
     for (let i = 0; i < database.length; i++){
         moodsHTML += `
             <div class="column">
-                <p>${database[i].name}</p>
+             <p class="mood-name">${database[i].name}</p>
                 <img src="${database[i].imageUrl}" alt="${database[i].name}">
-                <div class="advice">
-                    <p>Macho Advice:</p>
+                <div class="advice-container">
+                    <p class="macho-advice-label">Macho Advice</p>
                     <div class="quotes">        
                         <blockquote>"${database[i].quotes[0]}"</blockquote>
-                         <blockquote>"${database[i].quotes[1]}"</blockquote>
+                        <blockquote>"${database[i].quotes[1]}"</blockquote>
                         </div>
                     </div>
                 </div>
